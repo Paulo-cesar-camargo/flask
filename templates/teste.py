e074df9faf81f11613db370c1ffa2ee8cce3ff2c
Paulo Cesar Camargo <116305888+Paulo-cesar-camargo@users.noreply.github.com>
2023-06-19 11:55:58 -0300

    ok

---
+++ templates/teste.py
@@ -0,0 +1,9 @@
+frutas = ['Maça','goiba','morango','pera']
+for fruta in frutas:
+    print(fruta)
+
+notas = {'Paulo':7.0, 'Samantha':8.0, 'Gabrielly':9.1, 'Beatriz':8.9}
+
+for chave, valor in notas.items():
+    print(chave)
+    print(valor)
